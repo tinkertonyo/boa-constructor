f5a896a2d8e794fcfb01c3cf99ee1442feb9eb89
riaan <none@none>
2000-09-03 18:50:52 +0000

    *** empty log message ***

--- Scripts/HelpScrpt.py
+++ Scripts/HelpScrpt.py
@@ -30,31 +30,55 @@ header = '''#-------------------------------------------------------------------
 # Copy the the files declared below into this script's directory and
 # execute the script. Copy HelpCompanions to the COmpanions directory
 
+# Currently set for 2.2.1's help
+
 # Search for htm file containing: 
 #   <title>wxWindows classes implemented in wxPython</title>
-wxPythonClasses = 'wx471.htm'
+wxPythonClasses = 'wx477.htm'
 
 # Search for htm file containing: 
 #   <title>Miscellaneous functions</title>
-wxPythonFuncs = 'wx395.htm'
+wxPythonFuncs = 'wx400.htm'
+
 pythonLibRef = 'genindex.html'
-pythonLibRefDir = 'lib/'
+pythonLibDir = 'lib/'
+pythonModRef = 'modindex.html'
 
 helpFileDef = "%sDocs = '%s'\n"
 
+def ripPythonDocs(splLst, dir, name_term):
+    dict = {}
+    for line in splLst:
+        if line[:9]=='<a href="':
+            idx = string.find(line, '">')
+            if idx != -1:
+                href = line[9:idx]
+                name = line[idx+2:]
+
+                idx = string.find(name, name_term)
+                if idx != -1:
+                    name = string.strip(name[:idx])
+                    dict[name] = dir+href
+    return dict
+
+
 fileClss = open(wxPythonClasses, 'r')
-fileFncs = open(wxPythonFuncs, 'r')
-fileLibRef = open(pythonLibRef, 'r')
-output = open('HelpCompanions.py', 'w')
-
 clssHtmlLines = map(lambda a: string.strip(a), fileClss.readlines())
-fncsHtmlLines = map(lambda a: string.strip(a), fileFncs.readlines())
-libRefHtml = fileLibRef.read()
-
 fileClss.close()
+
+fileFncs = open(wxPythonFuncs, 'r')
+fncsHtmlLines = map(lambda a: string.strip(a), fileFncs.readlines())
 fileFncs.close()
+
+fileLibRef = open(pythonLibRef, 'r')
+libRefHtml = fileLibRef.read()
 fileLibRef.close()
 
+fileModRef = open(pythonModRef, 'r')
+libModHtml = fileModRef.read()
+fileModRef.close()
+
+output = open('HelpCompanions.py', 'w')
 output.write(header % strftime('%Y/%d/%m', gmtime(time())))
 
 clssHelpItems = clssHtmlLines[clssHtmlLines.index('<UL>')+1: clssHtmlLines.index('</UL>')]
@@ -86,32 +110,41 @@ for item in fncsHelpItems:
     href, name = string.split(item, '">::')
     output.write(helpFileDef % (name[:-8], wxPythonFuncs+href[9:]))
 
-libRefHelpItems = []
+#libRefHelpItems = []
 splLst = string.split(libRefHtml, '<dt>')
-libRefDict = {}
-for line in splLst:
-    if line[:9]=='<a href="':
-        idx = string.find(line, '">')
-        if idx != -1:
-            href = line[9:idx]
-            name = line[idx+2:]
 
-            idx = string.find(name, '()')
-            if idx != -1:
-                name = name[:idx]
-                libRefDict[name] = pythonLibRefDir+href
-#                print 'adding', href, 'to', name
+libRefDict = ripPythonDocs(string.split(libRefHtml, '<dt>'), pythonLibDir, '()')
+
+##for line in splLst:
+##    if line[:9]=='<a href="':
+##        idx = string.find(line, '">')
+##        if idx != -1:
+##            href = line[9:idx]
+##            name = line[idx+2:]
+##
+##            idx = string.find(name, '()')
+##            if idx != -1:
+##                name = name[:idx]
+##                libRefDict[name] = pythonLibRefDir+href
+###                print 'adding', href, 'to', name
 
 output.write('\n# Library reference index\n')
 output.write('libRefDocs = ')                  
 pprint.pprint(libRefDict, output) 
+
+modRefDict = ripPythonDocs(string.split(libModHtml, '<dt>'), pythonLibDir, '<')
+
+output.write('\n# Module index\n')
+output.write('modRefDocs = ')                  
+pprint.pprint(modRefDict, output) 
+
 #filter(lambda x: x[:9]=='<a href="', sgs)
 #libRefHelpItems
 
 
 output.close()
 
-
+print 'fin'
 
 
 
