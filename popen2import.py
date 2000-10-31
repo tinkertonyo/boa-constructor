b2127a1cbdaa4ead73d26448da2f537bf66d242c
riaan <none@none>
2000-10-31 03:00:57 +0000

    Module to hide away all the popen variants

---
+++ popen2import.py
@@ -0,0 +1,18 @@
+from wxPython import wx 
+import sys
+
+if sys.version[:2] == '2.':
+    from os import popen3
+elif wx.wxPlatform == '__WXMSW__':
+    
+    try:
+        from win32pipe import popen3
+    except ImportError:
+        wx.wxMessageBox('Please install Mark Hammond''s win32all package',
+              'Import Error', wx.wxOK | wx.wxICON_EXCLAMATION)
+        raise 
+else:
+    def popen3(cmd):
+        import popen2
+        o, i, e = popen2.popen3(cmd)
+        return i, o, e
