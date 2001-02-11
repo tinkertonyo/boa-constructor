d94ac4b086e830df0300eeeab8ebc7a1e3f7f13a
riaan <none@none>
2001-02-11 12:07:07 +0000

    Removed verbose copyright and worked around lincense() requiring keyboard input

--- ShellEditor.py
+++ ShellEditor.py
@@ -14,20 +14,43 @@ from wxPython.wx import *
 from Views.StyledTextCtrls import PythonStyledTextCtrlMix
 from wxPython.stc import *
 from ExternalLib.PythonInterpreter import PythonInterpreter
-import string
+import string, os
 import Preferences
+
+echo = true
 
 ps1 = '>>> '
 ps2 = '... '
-
+p2c = 'Type "copyright", "credits" or "license" for more information.'
+        
 class ShellEditor(wxStyledTextCtrl, PythonStyledTextCtrlMix):
     def __init__(self, parent, wId):
         wxStyledTextCtrl.__init__(self, parent, wId, style = wxCLIP_CHILDREN)
         PythonStyledTextCtrlMix.__init__(self, wId, -1)
+        #self.SetCaretPeriod(0)
 
         self.interp = PythonInterpreter()
+
+        try:
+            license
+        except NameError:
+            pass
+        else:
+            class MyLicensePrinter(license.__class__):
+                def __init__(self): 
+                    pass
+                def __call__(self):
+                    license._Printer__setup()
+                    print "'''\n%s\n'''"%string.join(license._Printer__lines, '\n')
+
+            self.interp.locals['license'] = MyLicensePrinter()
+            
         self.SetLexer(wxSTC_LEX_NULL)
-        self.SetText('# Python %s (Boa)\n# %s\n%s'%(sys.version, sys.copyright, ps1))
+        if sys.hexversion < 0x01060000:
+            copyright = sys.copyright
+        else:
+            copyright = p2c
+        self.SetText('# Python %s (Boa)\n# %s\n%s'%(sys.version, copyright, ps1))
         self.SetLexer(wxSTC_LEX_PYTHON)
         self.SetCurrentPosition(self.GetTextLength())
 ##        self.EnsureCaretVisible()
@@ -40,7 +63,6 @@ class ShellEditor(wxStyledTextCtrl, PythonStyledTextCtrlMix):
                 tmpstdout = sys.stdout
                 tmpstderr = sys.stderr
                 line = string.rstrip(self.GetLine(self.GetLineCount() -2)[4:-1])
-                print 'shell line', `line`
                 sys.stdout = PseudoFileOut(self)
                 sys.stderr = PseudoFileErr(self)
 
@@ -62,7 +84,8 @@ class ShellEditor(wxStyledTextCtrl, PythonStyledTextCtrlMix):
 
 class PseudoFile:
     """ Base class for file like objects to facilitate StdOut for the Shell."""
-    def __init__(self, output):
+    def __init__(self, output = None):
+        if output is None: output = []
         self.output = output
 
     def writelines(self, l):
@@ -75,7 +98,7 @@ class PseudoFile:
         pass
 
 class PseudoFileOut(PseudoFile):
-    tags = 'stderr'
+    tags = 'stdout'
     def write(self, s):
         self.output.SetLexer(wxSTC_LEX_NULL)
         self.output.AddText(s)
@@ -83,19 +106,29 @@ class PseudoFileOut(PseudoFile):
         self.output.Refresh(false)
 
 class PseudoFileErr(PseudoFile):
-    tags = 'stdout'
+    tags = 'stderr'
     def write(self, s):
         self.output.SetLexer(wxSTC_LEX_NULL)
         self.output.AddText(s)
         self.output.SetLexer(wxSTC_LEX_PYTHON)
         self.output.Refresh(false)
 
+class PseudoFileOutStore(PseudoFile):
+    tags = 'stdout'
+    def write(self, s):
+        self.output.append(s)
+    
+    def read(self):
+        return string.join(self.output, '')
+
 class PseudoFileOutTC(PseudoFile):
     tags = 'stderr'
     def write(self, s):
         self.output.AppendText(s)
+        if echo: sys.__stdout__.write(s)
 
 class PseudoFileErrTC(PseudoFile):
     tags = 'stdout'
     def write(self, s):
         self.output.AppendText(s)
+        if echo: sys.__stderr__.write(s)
