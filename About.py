580ae8636513ad41eee2e1532914a09cf3424d4b
riaan <none@none>
2001-02-11 09:14:47 +0000

    Changed about box to also be able to double as splash screen

--- About.py
+++ About.py
@@ -1,4 +1,3 @@
-#Boa:Dialog:AboutBox
 
 import __version__
 __version__ = __version__.ver
@@ -9,7 +8,7 @@ import wxPython.lib.wxpTag
 import Preferences
 
 #"#AC76DE"
-about_text = '''
+about_html = '''
 <hmtl>
 <body bgcolor="#99ccff">
 <center>
@@ -20,6 +19,12 @@ about_text = '''
     <font color="#006600">Constructor v%s</font></h2></td>
   </tr>
 </table>
+%s
+</body>
+</html>
+'''
+
+about_text = '''
 <p>A RAD GUI building IDE for <b>wxPython</b>
 </p>
 <p>&copy;2000 <b>Riaan Booysen</b>.<br>
@@ -39,10 +44,8 @@ riaan@e.co.za</p>
 <font size=-1><i>for <font color="#AA0000"><b>Bonnie</b></font></i></font>
 </center>
 </p>
-</body>
-</html>
+</p>
 '''
-
 ##&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 ##&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp
 ##<wxp class="wxButton">
@@ -64,50 +67,51 @@ riaan@e.co.za</p>
 ##
 
 
-def create(parent):
-    return wxDialog1(parent)
+def createSplash(parent):
+    return AboutBoxSplash(parent)
+
+def createNormal(parent):
+    return AboutBox(parent)
 
 [wxID_ABOUTBOX] = map(lambda _init_ctrls: wxNewId(), range(1))
 
-class AboutBox(wxDialog):
-
-    def _init_utils(self): 
-        pass
-
-    def _init_ctrls(self, prnt): 
-        wxDialog.__init__(self, size = wxSize(380,525), pos = (-1, -1), id = wxID_ABOUTBOX, title = 'About Boa Constructor', parent = prnt, name = 'AboutBox', style = wxDEFAULT_DIALOG_STYLE)        
-        
+class AboutBoxMixin:
     def __init__(self, parent): 
-        self._init_utils()
         self._init_ctrls(parent)
 
-	self.SetBackgroundColour(wxBLACK)
-	self.html = wxHtmlWindow(self, -1 )
-	self.html.SetPage(about_text % (Preferences.toPyPath('Images/Shared/Boa.jpg'),
-	  __version__, wxPlatform, wxMAJOR_VERSION, wxMINOR_VERSION, wxRELEASE_NUMBER))
-	self.SetAutoLayout(true)
-	lc = wxLayoutConstraints()
-	lc.top.SameAs(self, wxTop, 5)
-	lc.left.SameAs(self, wxLeft, 5)
-	lc.bottom.SameAs(self, wxBottom, 5)
-	lc.right.SameAs(self, wxRight, 5)
-	self.html.SetConstraints(lc)
-	self.Layout()
-	self.Center(wxBOTH)
-#        EVT_BUTTON(self, wxID_OK, self.OnOk)
+        self.SetBackgroundColour(wxBLACK)
+        self.html = wxHtmlWindow(self, -1 )
+        self.setPage()
+        self.SetAutoLayout(true)
+        lc = wxLayoutConstraints()
+        lc.top.SameAs(self, wxTop, 5)
+        lc.left.SameAs(self, wxLeft, 5)
+        lc.bottom.SameAs(self, wxBottom, 5)
+        lc.right.SameAs(self, wxRight, 5)
+        self.html.SetConstraints(lc)
+        self.Layout()
+        self.Center(wxBOTH)
         EVT_BUTTON(self, wxID_YES, self.OnNews)
-#	EVT_CLOSE(self, self.OnCloseWindow)
         self.SetAcceleratorTable(wxAcceleratorTable([(0, WXK_ESCAPE, wxID_OK),]))
     
     def OnNews(self, event):
         self.html.LoadPage('http://boa-constructor.sourceforge.net/News.html')
 
-##    def OnCloseWindow(self, event):
-##        self.Destroy()
-##        event.Skip()
+class AboutBox(AboutBoxMixin, wxDialog):
+    def _init_ctrls(self, prnt): 
+        wxDialog.__init__(self, size = wxSize(380,525), pos = (-1, -1), id = wxID_ABOUTBOX, title = 'About Boa Constructor', parent = prnt, name = 'AboutBox', style = wxDEFAULT_DIALOG_STYLE)        
+    
+    def setPage(self):
+        self.html.SetPage((about_html % (Preferences.toPyPath('Images/Shared/Boa.jpg'),
+          __version__, about_text % (wxPlatform, wxMAJOR_VERSION, wxMINOR_VERSION, wxRELEASE_NUMBER))))
 
+class AboutBoxSplash(AboutBoxMixin, wxFrame):
+    def _init_ctrls(self, prnt): 
+        wxFrame.__init__(self, size = wxSize(380,265), pos = (-1, -1), id = wxID_ABOUTBOX, title = 'About Boa Constructor', parent = prnt, name = 'AboutBox', style = wxSIMPLE_BORDER)        
 
-
+    def setPage(self):
+        self.html.SetPage(about_html % (Preferences.toPyPath('Images/Shared/Boa.jpg'),
+          __version__, ''))
 
 
 
