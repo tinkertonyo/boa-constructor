dd3b8c770119b6eb69226ff9dd06e86333178c70
riaan <none@none>
2001-04-08 22:56:10 +0000

    Module which stores the Palette data

---
+++ PaletteStore.py
@@ -0,0 +1,13 @@
+paletteLists = {'New': [],
+                'Dialogs': [],
+                'Zope': [],
+}
+
+palette = []
+
+newPalette = ['New', 'Editor/Tabs/New', paletteLists['New']]
+dialogPalette =  ['Dialogs', 'Editor/Tabs/Dialogs', paletteLists['Dialogs']]
+zopePalette =  ['Zope', 'Editor/Tabs/Zope', paletteLists['Zope']]
+
+helperClasses = {}
+compInfo = {}
