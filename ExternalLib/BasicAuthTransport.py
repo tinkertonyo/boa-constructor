221d17a097bb7c723b9ff2e26ae0adc779a2260a
riaan <none@none>
2001-06-13 21:03:02 +0000

    XML-RPC support libraries

---
+++ ExternalLib/BasicAuthTransport.py
@@ -0,0 +1,28 @@
+from base64 import *
+import string
+import httplib
+import xmlrpclib
+
+class BasicAuthTransport(xmlrpclib.Transport):
+    def __init__(self,username=None,password=None):
+        self.username=username
+        self.password=password
+    def request(self,host,handler,request_body):
+        h=httplib.HTTP(host)
+        h.putrequest("POST",handler)
+        h.putheader("Host",host)
+        h.putheader("User-Agent",self.user_agent)
+        h.putheader("Content-Type","text/xml")
+        h.putheader("Content-Length",str(len(request_body)))
+        if self.username is not None and self.password is not None:
+            h.putheader("AUTHORIZATION", "Basic %s" % string.replace(encodestring("%s:%s" % (self.username, self.password)),"\012", ""))
+        h.endheaders()
+        if request_body:
+            h.send(request_body)
+
+        errcode, errmsg, headers = h.getreply()
+        #print 'getreply ok'
+
+        if errcode != 200:
+            raise xmlrpclib.ProtocolError(host + handler,errcode,errmsg,headers)
+        return self.parse_response(h.getfile())
