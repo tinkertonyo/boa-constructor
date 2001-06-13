bb8effa6395c94210ee0872917fe86970ee678c6
riaan <none@none>
2001-06-13 21:06:25 +0000

    WebDAV client support copied from Zope

---
+++ ExternalLib/WebDAV/App_Common.py
@@ -0,0 +1,175 @@
+##############################################################################
+#
+# Zope Public License (ZPL) Version 1.0
+# -------------------------------------
+#
+# Copyright (c) Digital Creations.  All rights reserved.
+#
+# This license has been certified as Open Source(tm).
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# 1. Redistributions in source code must retain the above copyright
+#    notice, this list of conditions, and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions, and the following disclaimer in
+#    the documentation and/or other materials provided with the
+#    distribution.
+#
+# 3. Digital Creations requests that attribution be given to Zope
+#    in any manner possible. Zope includes a "Powered by Zope"
+#    button that is installed by default. While it is not a license
+#    violation to remove this button, it is requested that the
+#    attribution remain. A significant investment has been put
+#    into Zope, and this effort will continue if the Zope community
+#    continues to grow. This is one way to assure that growth.
+#
+# 4. All advertising materials and documentation mentioning
+#    features derived from or use of this software must display
+#    the following acknowledgement:
+#
+#      "This product includes software developed by Digital Creations
+#      for use in the Z Object Publishing Environment
+#      (http://www.zope.org/)."
+#
+#    In the event that the product being advertised includes an
+#    intact Zope distribution (with copyright and license included)
+#    then this clause is waived.
+#
+# 5. Names associated with Zope or Digital Creations must not be used to
+#    endorse or promote products derived from this software without
+#    prior written permission from Digital Creations.
+#
+# 6. Modified redistributions of any form whatsoever must retain
+#    the following acknowledgment:
+#
+#      "This product includes software developed by Digital Creations
+#      for use in the Z Object Publishing Environment
+#      (http://www.zope.org/)."
+#
+#    Intact (re-)distributions of any official Zope release do not
+#    require an external acknowledgement.
+#
+# 7. Modifications are encouraged but must be packaged separately as
+#    patches to official Zope releases.  Distributions that do not
+#    clearly separate the patches from the original work must be clearly
+#    labeled as unofficial distributions.  Modifications which do not
+#    carry the name Zope may be packaged in any form, as long as they
+#    conform to all of the clauses above.
+#
+#
+# Disclaimer
+#
+#   THIS SOFTWARE IS PROVIDED BY DIGITAL CREATIONS ``AS IS'' AND ANY
+#   EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+#   PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL DIGITAL CREATIONS OR ITS
+#   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+#   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+#   USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+#   ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+#   OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+#   OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+#   SUCH DAMAGE.
+#
+#
+# This software consists of contributions made by Digital Creations and
+# many individuals on behalf of Digital Creations.  Specific
+# attributions are listed in the accompanying credits file.
+#
+##############################################################################
+
+"""Commonly used utility functions."""
+
+__version__='$Revision$'[11:-2]
+
+import sys, os, time
+from string import rfind
+
+
+# These are needed because the various date formats below must
+# be in english per the RFCs. That means we can't use strftime,
+# which is affected by different locale settings.
+weekday_abbr = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+weekday_full = ['Monday', 'Tuesday', 'Wednesday', 'Thursday',
+                'Friday', 'Saturday', 'Sunday']
+monthname    = [None, 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+
+def iso8601_date(ts=None):
+    # Return an ISO 8601 formatted date string, required
+    # for certain DAV properties.
+    # '2000-11-10T16:21:09-08:00
+    if ts is None: ts=time.time()
+    return time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime(ts))
+
+def rfc850_date(ts=None):
+    # Return an HTTP-date formatted date string.
+    # 'Friday, 10-Nov-00 16:21:09 GMT'
+    if ts is None: ts=time.time()
+    year, month, day, hh, mm, ss, wd, y, z = time.gmtime(ts)
+    return "%s, %02d-%3s-%2s %02d:%02d:%02d GMT" % (
+            weekday_full[wd],
+            day, monthname[month],
+            str(year)[2:],
+            hh, mm, ss)
+
+def rfc1123_date(ts=None):
+    # Return an RFC 1123 format date string, required for
+    # use in HTTP Date headers per the HTTP 1.1 spec.
+    # 'Fri, 10 Nov 2000 16:21:09 GMT'
+    if ts is None: ts=time.time()
+    year, month, day, hh, mm, ss, wd, y, z = time.gmtime(ts)
+    return "%s, %02d %3s %4d %02d:%02d:%02d GMT" % (weekday_abbr[wd],
+                                                    day, monthname[month],
+                                                    year,
+                                                    hh, mm, ss)
+
+def absattr(attr, c=callable):
+    # Return the absolute value of an attribute,
+    # calling the attr if it is callable.
+    if c(attr):
+        return attr()
+    return attr
+
+def aq_base(ob, hasattr=hasattr):
+    # Return the aq_base of an object.
+    if hasattr(ob, 'aq_base'):
+        return ob.aq_base
+    return ob
+
+def is_acquired(ob, hasattr=hasattr, aq_base=aq_base, absattr=absattr):
+    # Return true if this object is not a direct
+    # subobject of its aq_parent object.
+    if not hasattr(ob, 'aq_parent'):
+        return 0
+    if hasattr(aq_base(ob.aq_parent), absattr(ob.id)):
+        return 0
+    if hasattr(aq_base(ob), 'isTopLevelPrincipiaApplicationObject') and \
+            ob.isTopLevelPrincipiaApplicationObject:
+        return 0
+    return 1
+
+
+def package_home(globals_dict):
+    __name__=globals_dict['__name__']
+    m=sys.modules[__name__]
+    if hasattr(m,'__path__'):
+        r=m.__path__[0]
+    elif "." in __name__:
+        r=sys.modules[__name__[:rfind(__name__,'.')]].__path__[0]
+    else:
+        r=__name__
+    return os.path.join(os.getcwd(), r)
+
+
+def attrget(o,name,default):
+    if hasattr(o,name): return getattr(o,name)
+    return default
+
+def Dictionary(**kw): return kw # Sorry Guido
