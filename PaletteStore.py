b469cb5fdaf87dfb2678fdb9eef9f6e5922c5979
riaan <none@none>
2001-07-15 14:41:05 +0000

    Added entry for controllers

--- PaletteStore.py
+++ PaletteStore.py
@@ -3,11 +3,11 @@ paletteLists = {'New': [],
                 'Zope': [],
 }
 
-palette = []
-
 newPalette = ['New', 'Editor/Tabs/New', paletteLists['New']]
+palette = []
 dialogPalette =  ['Dialogs', 'Editor/Tabs/Dialogs', paletteLists['Dialogs']]
 zopePalette =  ['Zope', 'Editor/Tabs/Zope', paletteLists['Zope']]
 
 helperClasses = {}
 compInfo = {}
+newControllers = {}
