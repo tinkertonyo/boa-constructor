1a78ad6aaf442128d788a3702f4b00962741ec4f
riaan <none@none>
2001-07-15 16:23:41 +0000

    Added some module descriptions

--- Boa.py
+++ Boa.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python  
 #----------------------------------------------------------------------
 # Name:        Boa.py
 # Purpose:     The main file for Boa.
@@ -7,14 +7,15 @@
 #
 # Created:     1999
 # RCS-ID:      $Id$
-# Copyright:   (c) 1999, 2000 Riaan Booysen
+# Copyright:   (c) 1999 - 2001 Riaan Booysen
 # Licence:     GPL
 #----------------------------------------------------------------------
 #Boa:App:BoaApp
 
 # Once upon a time
-# in a few files far away
 # the story starts
+# in a few files far away
+
 
 import sys, os, string
 
@@ -125,50 +126,70 @@ if Preferences.installBCRTL and hasattr(wxPython, '__file__'):
 
 # XXX Save as after app is closed
 
-modules ={'About': [0, '', 'About.py'],
- 'AppModuleProps': [0, '', 'AppModuleProps.py'],
- 'AppViews': [0, '', 'Views/AppViews.py'],
+modules ={'About': [0, 'About box and Splash screen', 'About.py'],
+ 'AppViews': [0, 'Views for the AppModel', 'Views/AppViews.py'],
  'BaseCompanions': [0, '', 'Companions/BaseCompanions.py'],
  'Browse': [0, 'History for navigation through the IDE', 'Browse.py'],
  'CVSExplorer': [0, '', 'Explorers/CVSExplorer.py'],
  'CVSResults': [0, '', 'Explorers/CVSResults.py'],
- 'ClassBrowser': [0, '', 'ClassBrowser.py'],
+ 'ClassBrowser': [0,
+                  'Frame that displays the wxPython object hierarchy by Class and Module',
+                  'ClassBrowser.py'],
  'CollectionEdit': [0, '', 'Views/CollectionEdit.py'],
- 'ComCompanions': [0, '', 'Companions/ComCompanions.py'],
- 'Companions': [0, '', 'Companions/Companions.py'],
- 'Constructors': [0, '', 'Companions/Constructors.py'],
+ 'ComCompanions': [0,
+                   'Companion classes for COM (win32 only)',
+                   'Companions/ComCompanions.py'],
+ 'Companions': [0,
+                'Most visual wxPython class companions ',
+                'Companions/Companions.py'],
+ 'Constructors': [0,
+                  'Constructor signature mixin classes',
+                  'Companions/Constructors.py'],
+ 'Controllers': [0, 'Controllers for the Models and Views', 'Controllers.py'],
  'CtrlAlign': [0, 'Aligns a group of controls', 'CtrlAlign.py'],
  'CtrlSize': [0, 'Sizes a group of controls', 'CtrlSize.py'],
  'DAVExplorer': [0, '', 'Explorers/DAVExplorer.py'],
- 'DataView': [0, '', 'Views/DataView.py'],
+ 'DataView': [0,
+              'View to manage non visual frame objects',
+              'Views/DataView.py'],
  'Debugger': [0,
               'Module for in-process debugging of wxPython and Python apps',
               'Debugger/Debugger.py'],
- 'Designer': [0, '', 'Views/Designer.py'],
+ 'Designer': [0, 'View to visually design frames', 'Views/Designer.py'],
  'DialogCompanions': [0, '', 'Companions/DialogCompanions.py'],
  'DiffView': [0, '', 'Views/DiffView.py'],
  'Editor': [0, 'Source code editor hosting models and views', 'Editor.py'],
  'EditorExplorer': [0, '', 'Explorers/EditorExplorer.py'],
- 'EditorHelper': [0, '', 'EditorHelper.py'],
- 'EditorModels': [0, '', 'EditorModels.py'],
- 'EditorUtils': [0, '', 'EditorUtils.py'],
- 'EditorViews': [0, '', 'Views/EditorViews.py'],
+ 'EditorHelper': [0, 'Module defining Editor constants', 'EditorHelper.py'],
+ 'EditorModels': [0,
+                  'Model classes. Models loosely represent source types.',
+                  'EditorModels.py'],
+ 'EditorUtils': [0,
+                 'Specialised ToolBar and StatusBar controls for the Editor',
+                 'EditorUtils.py'],
+ 'EditorViews': [0,
+                 'Main module for View classes that work with Models',
+                 'Views/EditorViews.py'],
  'Enumerations': [0, '', 'PropEdit/Enumerations.py'],
- 'ErrorStack': [0, '', 'ErrorStack.py'],
+ 'ErrorStack': [0, 'Various forms of error parsers', 'ErrorStack.py'],
  'ErrorStackFrm': [0, '', 'ErrorStackFrm.py'],
  'EventCollections': [0, '', 'Companions/EventCollections.py'],
- 'Explorer': [0, '', 'Explorers/Explorer.py'],
+ 'Explorer': [0,
+              'Specialised visual controls for the Explorer (Tree, list and splitter)',
+              'Explorers/Explorer.py'],
  'ExplorerNodes': [0, '', 'Explorers/ExplorerNodes.py'],
- 'ExtMethDlg': [0, '', 'ZopeLib/ExtMethDlg.py'],
+ 'ExtMethDlg': [0, 'Dialog for ExternalMethods', 'ZopeLib/ExtMethDlg.py'],
  'FTPExplorer': [0, '', 'Explorers/FTPExplorer.py'],
- 'FileDlg': [0, '', 'FileDlg.py'],
+ 'FileDlg': [0, 'Replacement for the standard file dialog. ', 'FileDlg.py'],
  'FileExplorer': [0, '', 'Explorers/FileExplorer.py'],
  'GCFrame': [0, '', 'GCFrame.py'],
  'HTMLCyclops': [0, '', 'HTMLCyclops.py'],
  'HTMLResponse': [0, '', 'HTMLResponse.py'],
- 'Help': [0, '', 'Help.py'],
+ 'Help': [0, 'Interactive help frame', 'Help.py'],
  'HelpCompanions': [0, '', 'Companions/HelpCompanions.py'],
- 'ImageStore': [0, '', 'ImageStore.py'],
+ 'ImageStore': [0,
+                'Centralised point to load images (cached/zipped/etc)',
+                'ImageStore.py'],
  'ImageViewer': [0, '', 'ZopeLib/ImageViewer.py'],
  'Infofields': [0, '', 'Infofields.py'],
  'InspectableViews': [0, '', 'Views/InspectableViews.py'],
@@ -177,52 +198,73 @@ modules ={'About': [0, '', 'About.py'],
                'Inspector.py'],
  'InspectorEditorControls': [0, '', 'PropEdit/InspectorEditorControls.py'],
  'LoginDialog': [0, '', 'ZopeLib/LoginDialog.py'],
- 'ModRunner': [0, '', 'ModRunner.py'],
+ 'ModRunner': [0,
+               'Module that runs processes in a variety of ways',
+               'ModRunner.py'],
  'OGLViews': [0, '', 'Views/OGLViews.py'],
  'ObjCollection': [0, '', 'Views/ObjCollection.py'],
- 'Palette': [1, '', 'Palette.py'],
+ 'Palette': [1,
+             'Top frame which hosts the component palette and help options',
+             'Palette.py'],
  'PaletteMapping': [0, '', 'PaletteMapping.py'],
+ 'PaletteStore': [0,
+                  'Storage for variables defining the palette organisation',
+                  'PaletteStore.py'],
  'PhonyApp': [0, '', 'PhonyApp.py'],
- 'Preferences': [0, '', 'Preferences.py'],
+ 'Preferences': [0,
+                 'Central store of customiseable properties',
+                 'Preferences.py'],
  'PrefsExplorer': [0, '', 'Explorers/PrefsExplorer.py'],
- 'PrefsGTK': [0, '', 'PrefsGTK.py'],
- 'PrefsKeys': [0, '', 'PrefsKeys.py'],
- 'PrefsMSW': [0, '', 'PrefsMSW.py'],
+ 'PrefsGTK': [0, 'Unix specific preference settings', 'PrefsGTK.py'],
+ 'PrefsKeys': [0, 'Keyboard bindings', 'PrefsKeys.py'],
+ 'PrefsMSW': [0, 'Windows specific preference settings', 'PrefsMSW.py'],
  'ProcessProgressDlg': [0, '', 'ProcessProgressDlg.py'],
  'ProfileView': [0, '', 'Views/ProfileView.py'],
  'PropDlg': [0, '', 'ZopeLib/PropDlg.py'],
- 'PropertyEditors': [0, '', 'PropEdit/PropertyEditors.py'],
+ 'PropertyEditors': [0,
+                     'Module defining property editors used in the Inspector',
+                     'PropEdit/PropertyEditors.py'],
  'PySourceView': [0, '', 'Views/PySourceView.py'],
  'PythonInterpreter': [0, '', 'ExternalLib/PythonInterpreter.py'],
- 'RTTI': [0, '', 'RTTI.py'],
+ 'RTTI': [0, 'Introspection code. Run time type info', 'RTTI.py'],
  'RunCyclops': [0, '', 'RunCyclops.py'],
  'SSHExplorer': [0, '', 'Explorers/SSHExplorer.py'],
  'Search': [0, '', 'Search.py'],
- 'SelectionTags': [0, '', 'Views/SelectionTags.py'],
- 'ShellEditor': [0, '', 'ShellEditor.py'],
+ 'SelectionTags': [0,
+                   'Controls and objects that manage the visual selection in the Designer',
+                   'Views/SelectionTags.py'],
+ 'ShellEditor': [0, 'Python Interpreter Shell window', 'ShellEditor.py'],
  'SourceViews': [0, '', 'Views/SourceViews.py'],
- 'StyledTextCtrls': [0, '', 'Views/StyledTextCtrls.py'],
+ 'StyledTextCtrls': [0,
+                     'Mixin classes to use features of Scintilla',
+                     'Views/StyledTextCtrls.py'],
  'Tests': [0, '', 'Tests.py'],
  'UserCompanions': [0, '', 'Companions/UserCompanions.py'],
  'UtilCompanions': [0, '', 'Companions/UtilCompanions.py'],
- 'Utils': [0, '', 'Utils.py'],
+ 'Utils': [0, 'General utility routines and classes', 'Utils.py'],
  'XMLView': [0, '', 'Views/XMLView.py'],
  'ZipExplorer': [0, '', 'Explorers/ZipExplorer.py'],
  'ZopeCompanions': [0, '', 'Companions/ZopeCompanions.py'],
- 'ZopeEditorModels': [0, '', 'ZopeEditorModels.py'],
+ 'ZopeEditorModels': [0,
+                      'Editor models for Zope objects',
+                      'ZopeEditorModels.py'],
  'ZopeExplorer': [0, '', 'Explorers/ZopeExplorer.py'],
  'ZopeFTP': [0, '', 'ZopeLib/ZopeFTP.py'],
  'ZopeViews': [0, '', 'Views/ZopeViews.py'],
- 'methodparse': [0, '', 'methodparse.py'],
- 'moduleparse': [0, '', 'moduleparse.py'],
+ 'methodparse': [0,
+                 'Module responsible for parsing code inside generated methods',
+                 'methodparse.py'],
+ 'moduleparse': [0,
+                 'For parsing a whole module into Module, classes and functions',
+                 'moduleparse.py'],
  'ndiff': [0, '', 'ExternalLib/ndiff.py'],
  'popen2import': [0, '', 'popen2import.py'],
  'relpath': [0, '', 'relpath.py'],
  'sender': [0, '', 'sender.py'],
- 'sourceconst': [0, '', 'sourceconst.py']}
+ 'sourceconst': [0, 'Source generation constants', 'sourceconst.py']}
 
 class BoaApp(wxApp):
-    """ Applcation object, responsible for the Splash screen, command line
+    """ Application object, responsible for the Splash screen, command line
         switches, optional logging and creation of the main frames. """
 
     def __init__(self, redirect=false):
@@ -297,3 +339,4 @@ def main():
 
 if __name__ == '__main__' or hasattr(wxApp, 'debugger'):
     main()
+ 
