2fbf749a566878628130a41a793e079e921b8ad5
riaan <none@none>
2001-07-15 14:02:17 +0000

    New controller id constants

--- EditorHelper.py
+++ EditorHelper.py
@@ -5,18 +5,29 @@ from wxPython.wx import wxNewId
  wxID_EDITORSWITCHAPP, wxID_DEFAULTVIEWS, wxID_EDITORSWITCHTO,
  wxID_EDITORTOGGLEVIEW, wxID_EDITORSWITCHEXPLORER, wxID_EDITORSWITCHSHELL,
  wxID_EDITORSWITCHPALETTE, wxID_EDITORSWITCHINSPECTOR, wxID_EDITORDIFF,
- wxID_EDITORCMPAPPS, wxID_EDITORHELPABOUT, wxID_EDITORPREVPAGE,
+ wxID_EDITORHELPABOUT, wxID_EDITORPREVPAGE,
  wxID_EDITORNEXTPAGE, wxID_EDITORBROWSEFORW, wxID_EDITORBROWSEBACK,
  wxID_EDITORHELPGUIDE, wxID_EDITORHELPTIPS,
  wxID_EDITORPYCHECK, wxID_EDITORCONFPYCHECK,
  wxID_EDITORRELOAD,
- wxID_SETUPINSTALL, wxID_SETUPCLEAN, wxID_SETUPBUILD,
+] = map(lambda _editor_menus: wxNewId(), range(27))
+
+[wxID_SETUPINSTALL, wxID_SETUPCLEAN, wxID_SETUPBUILD,
  wxID_SETUPSDIST, wxID_SETUPBDIST, wxID_SETUPBDIST_WININST, wxID_SETUPPY2EXE,
-] = map(lambda _editor_menus: wxNewId(), range(35))
+] = map(lambda _editor_setup: wxNewId(), range(7))
+
+[wxID_MODULEPROFILE, wxID_MODULECHECKSOURCE, wxID_MODULERUNAPP, wxID_MODULERUN,   
+ wxID_MODULERUNPARAMS, wxID_MODULEDEBUG, wxID_MODULEDEBUGPARAMS, 
+ wxID_MODULEDEBUGSTEPIN, wxID_MODULEDEBUGSTEPOVER, wxID_MODULEDEBUGSTEPOUT,
+ wxID_MODULECYCLOPSE, wxID_MODULEREINDENT
+] = map(lambda _editor_module: wxNewId(), range(12))
+
+[wxID_APPCMPAPPS, wxID_APPCRASHLOG,
+]= map(lambda _editor_app: wxNewId(), range(2))
 
 # Indexes for the imagelist
 imgCounter=27
-[imgFolder, imgFolderUp, imgPathFolder, imgCVSFolder, imgZopeSystemObj, 
+[imgFolderUp, imgFolder, imgPathFolder, imgCVSFolder, imgZopeSystemObj, 
  imgZopeConnection, imgBoaLogo, imgFSDrive, imgFolderBookmark,
 
  imgAppModel, imgFrameModel, imgDialogModel, imgMiniFrameModel,
