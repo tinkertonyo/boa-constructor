e20a9fb5fb150a90cf31f2b15ff3c0c2fa4c9a6f
riaan <none@none>
2001-07-28 21:21:34 +0000

    Added info to package

--- bcrtl/__init__.py
+++ bcrtl/__init__.py
@@ -1,1 +1,3 @@
-# Package initialisation
+# This is the Boa Constructor Run Time Library
+# Modules in this package are independent of Boa but available on the
+# Palette as components additional to wxPython.
