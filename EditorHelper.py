9fa8a92551ba7740e580c0355763622cbef17df8
riaan <none@none>
2001-09-04 23:07:17 +0000

    Added PyApp identifier, cleanup.

--- EditorHelper.py
+++ EditorHelper.py
@@ -1,8 +1,8 @@
-from wxPython.wx import wxNewId
+import Preferences, Utils
 
-[wxID_EDITOROPEN, wxID_EDITORSAVE, wxID_EDITORSAVEAS, wxID_EDITORCLOSEPAGE,
+(wxID_EDITOROPEN, wxID_EDITORSAVE, wxID_EDITORSAVEAS, wxID_EDITORCLOSEPAGE,
  wxID_EDITORREFRESH, wxID_EDITORDESIGNER, wxID_EDITORDEBUG, wxID_EDITORHELP,
- wxID_EDITORSWITCHAPP, wxID_DEFAULTVIEWS, wxID_EDITORSWITCHTO,
+ wxID_EDITORSWITCHAPP, wxID_DEFAULTVIEWS, wxID_EDITORSWITCHTO, wxID_EDITORADDTOAPP,
  wxID_EDITORTOGGLEVIEW, wxID_EDITORSWITCHEXPLORER, wxID_EDITORSWITCHSHELL,
  wxID_EDITORSWITCHPALETTE, wxID_EDITORSWITCHINSPECTOR, wxID_EDITORDIFF,
  wxID_EDITORHELPABOUT, wxID_EDITORHELPGUIDE, wxID_EDITORHELPTIPS, 
@@ -10,34 +10,33 @@ from wxPython.wx import wxNewId
  wxID_EDITORPREVPAGE, wxID_EDITORNEXTPAGE, 
  wxID_EDITORBROWSEFORW, wxID_EDITORBROWSEBACK,
  wxID_EDITORPYCHECK, wxID_EDITORCONFPYCHECK,
- wxID_EDITORRELOAD, wxID_EDITORATTACHTODEBUGGER
-] = map(lambda _editor_menus: wxNewId(), range(29))
+ wxID_EDITORRELOAD, wxID_EDITORATTACHTODEBUGGER, wxID_EDITOREXITBOA,
+) = Utils.wxNewIds(31)
 
-[wxID_SETUPINSTALL, wxID_SETUPCLEAN, wxID_SETUPBUILD,
+(wxID_SETUPINSTALL, wxID_SETUPCLEAN, wxID_SETUPBUILD,
  wxID_SETUPSDIST, wxID_SETUPBDIST, wxID_SETUPBDIST_WININST, wxID_SETUPPY2EXE,
-] = map(lambda _editor_setup: wxNewId(), range(7))
+) = Utils.wxNewIds(7)
 
-[wxID_MODULEPROFILE, wxID_MODULECHECKSOURCE, wxID_MODULERUNAPP, wxID_MODULERUN,   
+(wxID_MODULEPROFILE, wxID_MODULECHECKSOURCE, wxID_MODULERUNAPP, wxID_MODULERUN,   
  wxID_MODULERUNPARAMS, wxID_MODULEDEBUG, wxID_MODULEDEBUGPARAMS, 
  wxID_MODULEDEBUGSTEPIN, wxID_MODULEDEBUGSTEPOVER, wxID_MODULEDEBUGSTEPOUT,
- wxID_MODULECYCLOPSE, wxID_MODULEREINDENT
-] = map(lambda _editor_module: wxNewId(), range(12))
+ wxID_MODULECYCLOPSE, wxID_MODULEREINDENT,
+) = Utils.wxNewIds(12)
 
-[wxID_APPSAVEALL, wxID_APPCMPAPPS, wxID_APPCRASHLOG,
-]= map(lambda _editor_app: wxNewId(), range(3))
+(wxID_APPSAVEALL, wxID_APPCMPAPPS, wxID_APPCRASHLOG) = Utils.wxNewIds(3)
 
 # Indexes for the imagelist
-imgCounter=27
-[imgFolderUp, imgFolder, imgPathFolder, imgCVSFolder, imgZopeSystemObj, 
- imgZopeConnection, imgBoaLogo, imgFSDrive, imgFolderBookmark,
+imgCounter=32
+(imgFolderUp, imgFolder, imgPathFolder, imgCVSFolder, imgZopeSystemObj, 
+ imgZopeConnection, imgBoaLogo, imgFSDrive, imgNetDrive, imgFolderBookmark,
+ imgOpenEditorModels, imgPrefsFolder, imgPrefsSTCStyles,
 
  imgAppModel, imgFrameModel, imgDialogModel, imgMiniFrameModel,
- imgMDIParentModel, imgMDIChildModel, imgModuleModel, imgPackageModel,
+ imgMDIParentModel, imgMDIChildModel, imgPyAppModel, imgModuleModel, imgPackageModel,
  imgTextModel, imgConfigFileModel, imgZopeExportFileModel, imgBitmapFileModel,
  imgZipFileModel, imgCPPModel, imgUnknownFileModel, imgHTMLFileModel,
  imgXMLFileModel, imgSetupModel,
- 
-] = range(imgCounter)
+) = range(imgCounter)
 
 # Registry of all modules {modelIdentifier : Model} (populated by EditorModels)
 modelReg = {}
