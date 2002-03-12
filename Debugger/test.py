2bc22d88e617aa43cce8609a0c556d995a601bd6
riaan <none@none>
2002-03-12 03:33:25 +0000

    Changes to make the New Debugger more stable/usable. Have been tested to successfully debug Pyhon, wxPython, Tkinter and Zope. Old Debugger code has been removed.

--- Debugger/test.py
+++ Debugger/test.py
@@ -1,4 +1,3 @@
-
 import sys, os
 print os.getcwd()
 print sys.path
@@ -13,7 +12,7 @@ def test1():
     print 'I'
     print 'am'
     print 'here.'
-    
+
 def test():
     print 'Hello'
     print 'World!'
@@ -24,23 +23,22 @@ def test():
     print 'Bye.'
     raise 'I refuse to finish!'
     print 'yeah.'
-    
-if __name__ == '__main__':
-  if 1:
-    test()
-  else:
-    import IsolatedDebugger
-    dc = IsolatedDebugger.DebuggerController()
-    id = dc.createServer()
-    conn = IsolatedDebugger.DebuggerConnection(dc, id)
-    conn.run('test()', globals(), locals())
-    for n in range(3):
-        print conn.getInteractionUpdate()
-        conn.set_step_over()
-    for n in range(6):
-        print conn.getInteractionUpdate()
-        conn.set_step()
-    print conn.getInteractionUpdate()
-    conn.set_continue()
-    print conn.getInteractionUpdate()
 
+if __name__ == '__main__':
+    if 1:
+        test()
+    else:
+        import IsolatedDebugger
+        dc = IsolatedDebugger.DebuggerController()
+        id = dc.createServer()
+        conn = IsolatedDebugger.DebuggerConnection(dc, id)
+        conn.run('test()', globals(), locals())
+        for n in range(3):
+            print conn.getInteractionUpdate()
+            conn.set_step_over()
+        for n in range(6):
+            print conn.getInteractionUpdate()
+            conn.set_step()
+        print conn.getInteractionUpdate()
+        conn.set_continue()
+        print conn.getInteractionUpdate()
