d388839697321676a397557888bfe95c6fb4e119
hathawsh <none@none>
2002-03-22 18:27:00 +0000

    Implemented scripts-only mode, which shows only script frames in the stack and only steps through scripts.  Works great so far.

--- Debugger/ZopeScriptDebugServer.py
+++ Debugger/ZopeScriptDebugServer.py
@@ -10,8 +10,11 @@ isPythonScriptMetaType = {
 
 class ZopeScriptDebugServer(DebugServer):
 
+    # scripts_only_mode is turned on to stop only in through-the-web scripts.
+    scripts_only_mode = 0
+
     def getFilenameAndLine(self, frame):
-        """Returns the filename and line number for the frame.
+        """Returns the filename and line number for the frame.  Invoked often.
 
         This implementation adjusts for Python scripts.
         """
@@ -42,4 +45,54 @@ class ZopeScriptDebugServer(DebugServer):
             modname = ''
         funcname = frame.f_code.co_name
         return modname, funcname
+
+    def isTraceable(self, frame):
+        """Indicates whether the debugger should step into the given frame.
+
+        Invoked often.
+        """
+        if self.scripts_only_mode:
+            filename = frame.f_code.co_filename
+            if not isPythonScriptMetaType(filename):
+                # Stop only in high-level scripts.
+                return 0
+        return DebugServer.isTraceable(self, frame)
+
+    def isAScriptFrame(self, frame):
+        """Indicates whether the given frame is a high-level script frame.
+        """
+        filename = frame.f_code.co_filename
+        return isPythonScriptMetaType(filename)
+
+    def getStackInfo(self):
+        """Returns a tuple describing the current stack.
+        """
+        exc_type, exc_value, stack, frame_stack_len = (
+            DebugServer.getStackInfo(self))
+
+        if self.scripts_only_mode:
+            # Filter non-script frames out of the stack.
+            new_stack = []
+            new_len = 0
+            for idx in range(len(stack)):
+                frame, lineno = stack[idx]
+                if self.isAScriptFrame(frame):
+                    new_stack.append((frame, lineno))
+                    if idx < frame_stack_len:
+                        new_len = new_len + 1
+            stack = new_stack
+            frame_stack_len = new_len
+
+        return exc_type, exc_value, stack, frame_stack_len
+
+    def afterHardBreakpoint(self, frame):
+        # Set a default stepping mode.
+        self.set_step()
+        # Choose scripts_only_mode based on whether the hard break was in
+        # a script or not.
+        if self.isAScriptFrame(frame):
+            self.scripts_only_mode = 1
+        else:
+            self.scripts_only_mode = 0
+
 
