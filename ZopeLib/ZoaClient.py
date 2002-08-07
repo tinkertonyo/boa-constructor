9b522dc33980dd3e6bb30413d82a443674c22309
riaan <none@none>
2002-08-07 00:07:57 +0000

    zoa installer code.

--- ZopeLib/ZoaClient.py
+++ ZopeLib/ZoaClient.py
@@ -1,16 +1,30 @@
-#from ZopeLib 
+#-----------------------------------------------------------------------------
+# Name:        ZoaClient.py
+# Purpose:     Nicer interface to Zope's Client.py. Also, the zoa installer
+#
+# Author:      Riaan Booysen
+#
+# Created:     2002
+# RCS-ID:      $Id$
+# Copyright:   (c) 2002
+# Licence:     <your licence>
+#-----------------------------------------------------------------------------
+import os, string
+
 import Client
 
 class ZClient:
-    def __init__(self, url, user, passwd, timeout=None):
+    def __init__(self, url, user, passwd, timeout=None, argnames=()):
         self.__url = url
         self.__user = user
         self.__passwd = passwd
         self.__timeout = timeout
+        self.__argnames = argnames
     def __request(self, methodname, *args, **kwargs):
-        msg, res = apply(Client.Function(self.__url+'/'+methodname, 
-                username=self.__user, password=self.__passwd, 
-                timeout=self.__timeout), args, kwargs)
+        msg, res = apply(Client.Function(self.__url+'/'+methodname,
+                  username=self.__user, password=self.__passwd,
+                  timeout=self.__timeout, arguments=self.__argnames),
+                args, kwargs)
         return res
     def __getattr__(self, name):
         return _MethodDisp(self.__request, name)
@@ -24,6 +38,39 @@ class _MethodDisp:
     def __call__(self, *args, **kwargs):
         return apply(self.__request, (self.__name,) + args, kwargs)
 
-if __name__ == '__main__':
-    z = ZClient('http://localhost:8080', 'riaan', 'riaan', 10)
-    print z.Projects.Boa.title_or_id()
+def installZopeScript(conninfo, name, body):
+    url, user, passwd = conninfo
+    z = ZClient(url, user, passwd)
+    try:
+        z.manage_addProduct.PythonScripts.manage_addPythonScript(id=name, file=body)
+    except Client.ServerError, error:
+        if error.http_code != 302:
+            raise
+
+def installZopeFolder(conninfo, name):
+    url, user, passwd = conninfo
+    z = ZClient(url, user, passwd)
+    z.manage_addFolder(id=name)
+
+def installFromFS(conninfo, filepath):
+    name = os.path.basename(filepath)
+    installZopeFolder(conninfo, name)
+
+    url, user, passwd = conninfo
+    conninfo = url+'/'+name, user, passwd
+    os.path.walk(filepath, installList, (filepath, conninfo) )
+
+def installList(info, dirname, names):
+    fsPath, (url, user, passwd) = info
+
+    reldir = dirname[len(fsPath)+1:]
+    if reldir:
+        url = string.join([url] + string.split(reldir, os.sep), '/')
+
+    info = (url, user, passwd)
+    for item in names:
+        name, ext = os.path.splitext(item)
+        if ext == '.py':
+            installZopeScript(info, name, open(os.path.join(dirname, item)).read())
+        else:
+            installZopeFolder(info, item)
