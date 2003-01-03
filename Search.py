71023a825af34c6a6073981d94cc9a28eb532d78
riaan <none@none>
2003-01-03 02:15:17 +0000

    Added code by Roman Yakovenko to build file lists to search.

--- Search.py
+++ Search.py
@@ -108,6 +108,82 @@ def findInFiles(parent, srchPath, pattern, callback = defaultProgressCallback, d
         if owndlg:
             dlg.Destroy()
 
+class _file_iter:
+    def __init__(self, folders, file_filter, bIncludeFilter = 1, bRecursive = 1):
+        """
+            folders - list of folders to go through. This list must not be empty
+                      otherwise LookupError will be thrown
+            file_filter - may be right name could be file filter by file
+                          extension, if file_filter is empty then all files will
+                          be included.
+            bIncludeFilter - this flag indicates how to treat file_filter. If
+                             bIncludeFilter == True then all files that meets
+                             file_filter criteria will be included to resulting
+                             list
+            bRecursive - whether to walk through directories in recursive way or
+                         not
+        """
+        self._folders = folders
+        if not self._folders:
+            raise LookupError("Root folder was not specified")
+        self._filters = [sExt.lower() for sExt in file_filter]
+        self._is_include_filter = bIncludeFilter
+        self._is_recursive = bRecursive
+        self._files = [] #resulting list
+
+    def _is_to_include(self, sFullFileName):
+        """This function will return true if file must be included and false if not"""
+        if not self._filters:
+            return 1 #all files must be included
+        tpFileNameOnly = os.path.split( sFullFileName )
+        sExt = '*.' + tpFileNameOnly[-1].split('.')[-1]
+        if sExt.lower() in self._filters:
+            #file extension within filters
+            #if _is_include_filter = 1 then file must be included
+            return self._is_include_filter
+        else:
+            #file extension not in filters
+            #if _is_include_filter = 1 then file must be skiped
+            return not self._is_include_filter
+
+    def _GetFolderFileLists(self, sFullFolderName):
+        """This function will return tuple(folders, files) where files is a list
+           all files, according to file_filter and folders is all subfolders
+           of given folders. All results are full names
+        """
+        lstFiles, lstFolders = [], []
+        #getting all files from folder
+        lstContents = os.listdir(sFullFolderName)
+        for sPath in lstContents:
+            #building full file name
+            sFullPath = os.path.join(sFullFolderName, sPath)
+            if os.path.isfile( sFullPath ) and self._is_to_include( sFullPath ):
+                lstFiles.append( sFullPath )
+            elif os.path.isdir( sFullPath ):
+                lstFolders.append( sFullPath )
+            else:
+                pass
+        return lstFolders, lstFiles
+
+    def _walk(self):
+        """This function will work through foldres and collect all files"""
+        lstFolders = self._folders[:]
+        while lstFolders:
+            sCurrFolder = lstFolders.pop(0)
+            lstToWalkFolders, lstFiles = self._GetFolderFileLists(sCurrFolder)
+            if self._is_recursive:
+                lstFolders.extend( lstToWalkFolders )
+            self._files.extend( lstFiles )
+
+    def __call__(self):
+        self._files = []
+        self._walk()
+        return self._files
+
+def listFiles(folders, file_filter, bIncludeFilter=1, bRecursive=1):
+    return _file_iter(folders, file_filter, bIncludeFilter, bRecursive)()
+
+
 if __name__ == '__main__':
     from wxPython.wx import *
     wxPySimpleApp()
