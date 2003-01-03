4f927613cd07ba096dbed123beea46387828ba7e
riaan <none@none>
2003-01-03 02:45:43 +0000

    Regenerated source.

--- Debugger/RemoteDialog.py
+++ Debugger/RemoteDialog.py
@@ -5,52 +5,76 @@ from wxPython.wx import *
 def create(parent):
     return AttachDlg(parent)
 
-[wxID_ATTACHDLGPORT_CTRL,
- wxID_ATTACHDLGHOST_CTRL,
- wxID_ATTACHDLGPASSWORD_CTRL,
- wxID_ATTACHDLGUSERNAME_CTRL,
- wxID_ATTACHDLGSTATICTEXT1,
- wxID_ATTACHDLGSTATICTEXT3,
- wxID_ATTACHDLGSTATICTEXT2,
- wxID_ATTACHDLGHELP_BUTTON,
- wxID_ATTACHDLGSTATICTEXT4,
- wxID_ATTACHDLGCANCEL_BUTTON,
- wxID_ATTACHDLGOK_BUTTON,
- wxID_ATTACHDLG] = map(lambda _init_ctrls: wxNewId(), range(12))
+[wxID_ATTACHDLG, wxID_ATTACHDLGCANCEL_BUTTON, wxID_ATTACHDLGHELP_BUTTON, 
+ wxID_ATTACHDLGHOST_CTRL, wxID_ATTACHDLGOK_BUTTON, 
+ wxID_ATTACHDLGPASSWORD_CTRL, wxID_ATTACHDLGPORT_CTRL, 
+ wxID_ATTACHDLGSTATICTEXT1, wxID_ATTACHDLGSTATICTEXT2, 
+ wxID_ATTACHDLGSTATICTEXT3, wxID_ATTACHDLGSTATICTEXT4, 
+ wxID_ATTACHDLGUSERNAME_CTRL, 
+] = map(lambda _init_ctrls: wxNewId(), range(12))
 
 class AttachDlg(wxDialog):
     def _init_utils(self):
+        # generated method, don't edit
         pass
 
     def _init_ctrls(self, prnt):
-        wxDialog.__init__(self, size = wxSize(288, 227), id = wxID_ATTACHDLG, title = 'Attach to debugger', parent = prnt, name = 'AttachDlg', style = wxDEFAULT_DIALOG_STYLE, pos = wxPoint(344, 237))
+        # generated method, don't edit
+        wxDialog.__init__(self, id=wxID_ATTACHDLG, name='AttachDlg',
+              parent=prnt, pos=wxPoint(344, 237), size=wxSize(288, 227),
+              style=wxDEFAULT_DIALOG_STYLE, title='Attach to debugger')
         self._init_utils()
+        self.SetClientSize(wxSize(280, 200))
         EVT_CLOSE(self, self.OnCloseWindow)
 
-        self.staticText1 = wxStaticText(label = 'Host', id = wxID_ATTACHDLGSTATICTEXT1, parent = self, name = 'staticText1', size = wxSize(56, 16), style = 0, pos = wxPoint(16, 24))
+        self.staticText1 = wxStaticText(id=wxID_ATTACHDLGSTATICTEXT1,
+              label='Host', name='staticText1', parent=self, pos=wxPoint(16,
+              24), size=wxSize(56, 16), style=0)
 
-        self.staticText2 = wxStaticText(label = 'Port', id = wxID_ATTACHDLGSTATICTEXT2, parent = self, name = 'staticText2', size = wxSize(56, 16), style = 0, pos = wxPoint(16, 56))
+        self.staticText2 = wxStaticText(id=wxID_ATTACHDLGSTATICTEXT2,
+              label='Port', name='staticText2', parent=self, pos=wxPoint(16,
+              56), size=wxSize(56, 16), style=0)
 
-        self.staticText3 = wxStaticText(label = 'Username', id = wxID_ATTACHDLGSTATICTEXT3, parent = self, name = 'staticText3', size = wxSize(56, 16), style = 0, pos = wxPoint(16, 88))
+        self.staticText3 = wxStaticText(id=wxID_ATTACHDLGSTATICTEXT3,
+              label='Username', name='staticText3', parent=self, pos=wxPoint(16,
+              88), size=wxSize(56, 16), style=0)
 
-        self.staticText4 = wxStaticText(label = 'Password', id = wxID_ATTACHDLGSTATICTEXT4, parent = self, name = 'staticText4', size = wxSize(56, 16), style = 0, pos = wxPoint(16, 120))
+        self.staticText4 = wxStaticText(id=wxID_ATTACHDLGSTATICTEXT4,
+              label='Password', name='staticText4', parent=self, pos=wxPoint(16,
+              120), size=wxSize(56, 16), style=0)
 
-        self.host_ctrl = wxTextCtrl(size = wxSize(160, 24), value = '127.0.0.1', pos = wxPoint(80, 16), parent = self, name = 'host_ctrl', style = 0, id = wxID_ATTACHDLGHOST_CTRL)
+        self.host_ctrl = wxTextCtrl(id=wxID_ATTACHDLGHOST_CTRL,
+              name='host_ctrl', parent=self, pos=wxPoint(80, 16),
+              size=wxSize(160, 24), style=0, value='127.0.0.1')
 
-        self.port_ctrl = wxTextCtrl(size = wxSize(48, 24), value = '26200', pos = wxPoint(80, 48), parent = self, name = 'port_ctrl', style = 0, id = wxID_ATTACHDLGPORT_CTRL)
+        self.port_ctrl = wxTextCtrl(id=wxID_ATTACHDLGPORT_CTRL,
+              name='port_ctrl', parent=self, pos=wxPoint(80, 48),
+              size=wxSize(48, 24), style=0, value='26200')
 
-        self.username_ctrl = wxTextCtrl(size = wxSize(96, 24), value = '', pos = wxPoint(80, 80), parent = self, name = 'username_ctrl', style = 0, id = wxID_ATTACHDLGUSERNAME_CTRL)
+        self.username_ctrl = wxTextCtrl(id=wxID_ATTACHDLGUSERNAME_CTRL,
+              name='username_ctrl', parent=self, pos=wxPoint(80, 80),
+              size=wxSize(96, 24), style=0, value='')
 
-        self.password_ctrl = wxTextCtrl(size = wxSize(96, 24), value = '', pos = wxPoint(80, 112), parent = self, name = 'password_ctrl', style = wxTE_PASSWORD, id = wxID_ATTACHDLGPASSWORD_CTRL)
+        self.password_ctrl = wxTextCtrl(id=wxID_ATTACHDLGPASSWORD_CTRL,
+              name='password_ctrl', parent=self, pos=wxPoint(80, 112),
+              size=wxSize(96, 24), style=wxTE_PASSWORD, value='')
 
-        self.ok_button = wxButton(label = 'Ok', id = wxID_ATTACHDLGOK_BUTTON, parent = self, name = 'ok_button', size = wxSize(72, 24), style = 0, pos = wxPoint(24, 160))
+        self.ok_button = wxButton(id=wxID_ATTACHDLGOK_BUTTON, label='Ok',
+              name='ok_button', parent=self, pos=wxPoint(24, 160),
+              size=wxSize(72, 24), style=0)
         EVT_BUTTON(self.ok_button, wxID_ATTACHDLGOK_BUTTON, self.OnOkButton)
 
-        self.cancel_button = wxButton(label = 'Cancel', id = wxID_ATTACHDLGCANCEL_BUTTON, parent = self, name = 'cancel_button', size = wxSize(72, 24), style = 0, pos = wxPoint(104, 160))
-        EVT_BUTTON(self.cancel_button, wxID_ATTACHDLGCANCEL_BUTTON, self.OnCancelButton)
+        self.cancel_button = wxButton(id=wxID_ATTACHDLGCANCEL_BUTTON,
+              label='Cancel', name='cancel_button', parent=self,
+              pos=wxPoint(104, 160), size=wxSize(72, 24), style=0)
+        EVT_BUTTON(self.cancel_button, wxID_ATTACHDLGCANCEL_BUTTON,
+              self.OnCancelButton)
 
-        self.help_button = wxButton(label = 'Help', id = wxID_ATTACHDLGHELP_BUTTON, parent = self, name = 'help_button', size = wxSize(72, 24), style = 0, pos = wxPoint(184, 160))
-        EVT_BUTTON(self.help_button, wxID_ATTACHDLGHELP_BUTTON, self.OnHelpButton)
+        self.help_button = wxButton(id=wxID_ATTACHDLGHELP_BUTTON, label='Help',
+              name='help_button', parent=self, pos=wxPoint(184, 160),
+              size=wxSize(72, 24), style=0)
+        EVT_BUTTON(self.help_button, wxID_ATTACHDLGHELP_BUTTON,
+              self.OnHelpButton)
 
     def __init__(self, editor):
         self._init_ctrls(editor)
