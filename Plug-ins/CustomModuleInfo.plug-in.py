293ffb47c193ab67791cc2a6da03f19b80d514da
riaan <none@none>
2003-01-03 05:05:09 +0000

    Plugin demonstrating how to change source constants in a plugin.

---
+++ Plug-ins/CustomModuleInfo.plug-in.py
@@ -0,0 +1,21 @@
+""" Demonstrates how to change system constants as a plug-in """
+
+import sourceconst
+# The order of (Name)s may change and lines may also be removed
+sourceconst.defInfoBlock = '''#-----------------------------------------------------------------------------
+# Name:        %(Name)s
+# Purpose:     %(Purpose)s
+#
+# Author:      %(Author)s
+#
+# Created:     %(Created)s
+# RCS-ID:      %(RCS-ID)s
+# Copyright:   %(Copyright)s
+# Licence:     %(Licence)s
+# New field:   %(NewField)s
+#-----------------------------------------------------------------------------
+'''
+
+import Preferences
+# (Name)s not in the original dictionary needs to be added
+Preferences.staticInfoPrefs['NewField'] = 'Whatever'
