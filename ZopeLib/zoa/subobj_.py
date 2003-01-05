79a00b1d74b96e285d12938e80d435f00d66c251
riaan <none@none>
2003-01-05 16:30:17 +0000

    Works around changes in 2.6

--- ZopeLib/zoa/subobj_.py
+++ ZopeLib/zoa/subobj_.py
@@ -10,9 +10,9 @@
 obj = context.aq_parent
 for subobj in obj.objectValues():
     try:
-        name = subobj.id()
-    except TypeError:
         name = subobj.id
+    except:
+        continue
     if name == objname:
         return subobj
 raise 'Method not found in %s'%obj.id
