89c90b32d9e4525638e03cc675555acdbd8bf9b5
riaan <none@none>
2003-01-08 13:14:05 +0000

    Fix for missing SetBitmap method on wxGTK 2.3.2

--- Utils.py
+++ Utils.py
@@ -910,6 +910,8 @@ def appendMenuItem(menu, wId, label, code=(), bmp='', help=''):
     text = label + (code and ' \t'+code[2] or '')
     menuItem = wxMenuItem(menu, wId, text, help)
     if Preferences.editorMenuImages and bmp and bmp != '-':
-        menuItem.SetBitmap(Preferences.IS.load(bmp))
+        if wxPlatform == '__WXGTK__' and wxVersion >= (2,3,3) or \
+              wxPlatform == '__WXMSW__':
+            menuItem.SetBitmap(Preferences.IS.load(bmp))
     menu.AppendItem(menuItem)
     
