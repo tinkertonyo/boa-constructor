eceb3ab2d1d9b3aac534a5d2e673ec1ac1bae654
riaan <none@none>
2003-04-01 15:36:48 +0000

    Whitespace cleanup

--- Companions/BasicCompanions.py
+++ Companions/BasicCompanions.py
@@ -293,61 +293,61 @@ class HtmlWindowDTC(HtmlWindowConstr, WindowDTC):
         return 'from wxPython.html import *'
 
 stcEOLMode = [wxSTC_EOL_CRLF, wxSTC_EOL_CR, wxSTC_EOL_LF]
-stcEOLModeNames = {'wxSTC_EOL_CRLF': wxSTC_EOL_CRLF, 
-                   'wxSTC_EOL_CR': wxSTC_EOL_CR, 
+stcEOLModeNames = {'wxSTC_EOL_CRLF': wxSTC_EOL_CRLF,
+                   'wxSTC_EOL_CR': wxSTC_EOL_CR,
                    'wxSTC_EOL_LF': wxSTC_EOL_LF}
 
 stcEdgeMode = [wxSTC_EDGE_NONE, wxSTC_EDGE_LINE, wxSTC_EDGE_BACKGROUND]
-stcEdgeModeNames = {'wxSTC_EDGE_NONE': wxSTC_EDGE_NONE, 
-                    'wxSTC_EDGE_LINE': wxSTC_EDGE_LINE, 
+stcEdgeModeNames = {'wxSTC_EDGE_NONE': wxSTC_EDGE_NONE,
+                    'wxSTC_EDGE_LINE': wxSTC_EDGE_LINE,
                     'wxSTC_EDGE_BACKGROUND': wxSTC_EDGE_BACKGROUND}
 
 stcLexer = [wxSTC_LEX_NULL, wxSTC_LEX_PYTHON, wxSTC_LEX_CONTAINER,
             wxSTC_LEX_CPP , wxSTC_LEX_HTML , wxSTC_LEX_XML, wxSTC_LEX_PERL,
-            wxSTC_LEX_SQL, wxSTC_LEX_VB, wxSTC_LEX_PROPERTIES, 
-            wxSTC_LEX_ERRORLIST, wxSTC_LEX_MAKEFILE, wxSTC_LEX_BATCH, 
+            wxSTC_LEX_SQL, wxSTC_LEX_VB, wxSTC_LEX_PROPERTIES,
+            wxSTC_LEX_ERRORLIST, wxSTC_LEX_MAKEFILE, wxSTC_LEX_BATCH,
             wxSTC_LEX_XCODE, wxSTC_LEX_LATEX, wxSTC_LEX_LUA, wxSTC_LEX_DIFF,
             wxSTC_LEX_CONF, wxSTC_LEX_PASCAL, wxSTC_LEX_AVE, wxSTC_LEX_ADA,
             wxSTC_LEX_LISP, wxSTC_LEX_RUBY, wxSTC_LEX_EIFFEL, wxSTC_LEX_EIFFELKW,
-            wxSTC_LEX_TCL, wxSTC_LEX_NNCRONTAB, wxSTC_LEX_BULLANT, 
+            wxSTC_LEX_TCL, wxSTC_LEX_NNCRONTAB, wxSTC_LEX_BULLANT,
             wxSTC_LEX_VBSCRIPT, wxSTC_LEX_ASP, wxSTC_LEX_PHP, wxSTC_LEX_BAAN,
             wxSTC_LEX_MATLAB, wxSTC_LEX_SCRIPTOL, wxSTC_LEX_AUTOMATIC]
 stcLexerNames = {'wxSTC_LEX_NULL': wxSTC_LEX_NULL,
-      'wxSTC_LEX_PYTHON': wxSTC_LEX_PYTHON, 
+      'wxSTC_LEX_PYTHON': wxSTC_LEX_PYTHON,
       'wxSTC_LEX_CONTAINER': wxSTC_LEX_CONTAINER,
-      'wxSTC_LEX_CPP': wxSTC_LEX_CPP, 'wxSTC_LEX_HTML': wxSTC_LEX_HTML, 
+      'wxSTC_LEX_CPP': wxSTC_LEX_CPP, 'wxSTC_LEX_HTML': wxSTC_LEX_HTML,
       'wxSTC_LEX_XML': wxSTC_LEX_XML, 'wxSTC_LEX_PERL': wxSTC_LEX_PERL,
       'wxSTC_LEX_SQL': wxSTC_LEX_SQL, 'wxSTC_LEX_VB': wxSTC_LEX_VB,
-      'wxSTC_LEX_PROPERTIES': wxSTC_LEX_PROPERTIES, 
+      'wxSTC_LEX_PROPERTIES': wxSTC_LEX_PROPERTIES,
       'wxSTC_LEX_ERRORLIST': wxSTC_LEX_ERRORLIST,
-      'wxSTC_LEX_MAKEFILE': wxSTC_LEX_MAKEFILE, 
-      'wxSTC_LEX_BATCH': wxSTC_LEX_BATCH, 
+      'wxSTC_LEX_MAKEFILE': wxSTC_LEX_MAKEFILE,
+      'wxSTC_LEX_BATCH': wxSTC_LEX_BATCH,
       'wxSTC_LEX_XCODE': wxSTC_LEX_XCODE, 'wxSTC_LEX_LATEX': wxSTC_LEX_LATEX,
-      'wxSTC_LEX_LUA': wxSTC_LEX_LUA, 'wxSTC_LEX_DIFF': wxSTC_LEX_DIFF, 
+      'wxSTC_LEX_LUA': wxSTC_LEX_LUA, 'wxSTC_LEX_DIFF': wxSTC_LEX_DIFF,
       'wxSTC_LEX_CONF': wxSTC_LEX_CONF, 'wxSTC_LEX_PASCAL': wxSTC_LEX_PASCAL,
       'wxSTC_LEX_AVE': wxSTC_LEX_AVE, 'wxSTC_LEX_ADA': wxSTC_LEX_ADA,
       'wxSTC_LEX_LISP': wxSTC_LEX_LISP, 'wxSTC_LEX_RUBY': wxSTC_LEX_RUBY,
-      'wxSTC_LEX_EIFFEL': wxSTC_LEX_EIFFEL, 
+      'wxSTC_LEX_EIFFEL': wxSTC_LEX_EIFFEL,
       'wxSTC_LEX_EIFFELKW': wxSTC_LEX_EIFFELKW,
       'wxSTC_LEX_TCL': wxSTC_LEX_TCL, 'wxSTC_LEX_NNCRONTAB': wxSTC_LEX_NNCRONTAB,
-      'wxSTC_LEX_BULLANT': wxSTC_LEX_BULLANT, 
+      'wxSTC_LEX_BULLANT': wxSTC_LEX_BULLANT,
       'wxSTC_LEX_VBSCRIPT': wxSTC_LEX_VBSCRIPT, 'wxSTC_LEX_ASP': wxSTC_LEX_ASP,
       'wxSTC_LEX_PHP': wxSTC_LEX_PHP, 'wxSTC_LEX_BAAN': wxSTC_LEX_BAAN,
-      'wxSTC_LEX_MATLAB': wxSTC_LEX_MATLAB, 
+      'wxSTC_LEX_MATLAB': wxSTC_LEX_MATLAB,
       'wxSTC_LEX_SCRIPTOL': wxSTC_LEX_SCRIPTOL,
       'wxSTC_LEX_AUTOMATIC': wxSTC_LEX_AUTOMATIC}
 
-stcPrintColourMode = [wxSTC_PRINT_NORMAL, wxSTC_PRINT_INVERTLIGHT, 
-      wxSTC_PRINT_BLACKONWHITE, wxSTC_PRINT_COLOURONWHITE, 
+stcPrintColourMode = [wxSTC_PRINT_NORMAL, wxSTC_PRINT_INVERTLIGHT,
+      wxSTC_PRINT_BLACKONWHITE, wxSTC_PRINT_COLOURONWHITE,
       wxSTC_PRINT_COLOURONWHITEDEFAULTBG]
-stcPrintColourModeNames = {'wxSTC_PRINT_NORMAL': wxSTC_PRINT_NORMAL, 
-      'wxSTC_PRINT_INVERTLIGHT': wxSTC_PRINT_INVERTLIGHT, 
-      'wxSTC_PRINT_BLACKONWHITE': wxSTC_PRINT_BLACKONWHITE, 
-      'wxSTC_PRINT_COLOURONWHITE': wxSTC_PRINT_COLOURONWHITE, 
+stcPrintColourModeNames = {'wxSTC_PRINT_NORMAL': wxSTC_PRINT_NORMAL,
+      'wxSTC_PRINT_INVERTLIGHT': wxSTC_PRINT_INVERTLIGHT,
+      'wxSTC_PRINT_BLACKONWHITE': wxSTC_PRINT_BLACKONWHITE,
+      'wxSTC_PRINT_COLOURONWHITE': wxSTC_PRINT_COLOURONWHITE,
       'wxSTC_PRINT_COLOURONWHITEDEFAULTBG': wxSTC_PRINT_COLOURONWHITEDEFAULTBG}
 
 stcWrapMode = [wxSTC_WRAP_NONE, wxSTC_WRAP_WORD]
-stcWrapModeNames = {'wxSTC_WRAP_NONE': wxSTC_WRAP_NONE, 
+stcWrapModeNames = {'wxSTC_WRAP_NONE': wxSTC_WRAP_NONE,
                     'wxSTC_WRAP_WORD': wxSTC_WRAP_WORD}
 
 class StyledTextCtrlDTC(WindowConstr, WindowDTC):
@@ -368,17 +368,17 @@ class StyledTextCtrlDTC(WindowConstr, WindowDTC):
              'ViewEOL': BoolPropEdit,
              'ViewWhiteSpace': BoolPropEdit,
              'EOLMode': EnumPropEdit,
-             'EdgeMode': EnumPropEdit, 
-             'Lexer': EnumPropEdit, 
+             'EdgeMode': EnumPropEdit,
+             'Lexer': EnumPropEdit,
              'PrintColourMode': EnumPropEdit,
              'WrapMode': EnumPropEdit,
             })
-             
+
         self.options.update({'EOLMode'   : stcEOLMode,
                              'EdgeMode' : stcEdgeMode,
                              'Lexer': stcLexer,
                              'PrintColourMode': stcPrintColourMode,
-                             'WrapMode': stcWrapMode,       
+                             'WrapMode': stcWrapMode,
                             })
         self.names.update({'EOLMode'   : stcEOLModeNames,
                            'EdgeMode' : stcEdgeModeNames,
@@ -386,7 +386,7 @@ class StyledTextCtrlDTC(WindowConstr, WindowDTC):
                            'PrintColourMode': stcPrintColourModeNames,
                            'WrapMode': stcWrapModeNames,
                           })
-             
+
     def designTimeSource(self, position = 'wxDefaultPosition', size = 'wxDefaultSize'):
         return {'pos': position,
                 'size': size,
@@ -394,7 +394,7 @@ class StyledTextCtrlDTC(WindowConstr, WindowDTC):
                 'name': `self.name`}
 
     def hideDesignTime(self):
-        return WindowDTC.hideDesignTime(self) + ['Anchor', 'CodePage', 
+        return WindowDTC.hideDesignTime(self) + ['Anchor', 'CodePage',
                'DocPointer', 'LastKeydownProcessed', 'ModEventMask',
                'Status', 'STCFocus']
 
