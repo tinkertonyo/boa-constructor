5334ea572958ff8ff2167452d8891202b0594019
riaan <none@none>
2003-07-10 10:20:18 +0000

    Made dialog default values persist across sessions.

--- Debugger/RemoteDialog.py
+++ Debugger/RemoteDialog.py
@@ -5,15 +5,20 @@ from wxPython.wx import *
 def create(parent):
     return AttachDlg(parent)
 
-[wxID_ATTACHDLG, wxID_ATTACHDLGCANCEL_BUTTON, wxID_ATTACHDLGHELP_BUTTON,
- wxID_ATTACHDLGHOST_CTRL, wxID_ATTACHDLGOK_BUTTON,
- wxID_ATTACHDLGPASSWORD_CTRL, wxID_ATTACHDLGPORT_CTRL,
- wxID_ATTACHDLGSTATICTEXT1, wxID_ATTACHDLGSTATICTEXT2,
- wxID_ATTACHDLGSTATICTEXT3, wxID_ATTACHDLGSTATICTEXT4,
- wxID_ATTACHDLGUSERNAME_CTRL,
-] = map(lambda _init_ctrls: wxNewId(), range(12))
+[wxID_ATTACHDLG, wxID_ATTACHDLGCANCEL_BUTTON, wxID_ATTACHDLGHELP_BUTTON, 
+ wxID_ATTACHDLGHOST_CTRL, wxID_ATTACHDLGOK_BUTTON, 
+ wxID_ATTACHDLGPASSWORD_CTRL, wxID_ATTACHDLGPORT_CTRL, 
+ wxID_ATTACHDLGREM_PASS_CTRL, wxID_ATTACHDLGSTATICTEXT1, 
+ wxID_ATTACHDLGSTATICTEXT2, wxID_ATTACHDLGSTATICTEXT3, 
+ wxID_ATTACHDLGSTATICTEXT4, wxID_ATTACHDLGUSERNAME_CTRL, 
+] = map(lambda _init_ctrls: wxNewId(), range(13))
 
 class AttachDlg(wxDialog):
+    rem_host = None
+    rem_port = None
+    rem_user = None
+    rem_pass = None
+    
     def _init_utils(self):
         # generated method, don't edit
         pass
@@ -45,19 +50,19 @@ class AttachDlg(wxDialog):
 
         self.host_ctrl = wxTextCtrl(id=wxID_ATTACHDLGHOST_CTRL,
               name='host_ctrl', parent=self, pos=wxPoint(80, 16),
-              size=wxSize(160, 24), style=0, value='127.0.0.1')
+              size=wxSize(160, 24), style=0, value=self.host)
 
         self.port_ctrl = wxTextCtrl(id=wxID_ATTACHDLGPORT_CTRL,
               name='port_ctrl', parent=self, pos=wxPoint(80, 48),
-              size=wxSize(48, 24), style=0, value='26200')
+              size=wxSize(48, 24), style=0, value=self.port)
 
         self.username_ctrl = wxTextCtrl(id=wxID_ATTACHDLGUSERNAME_CTRL,
               name='username_ctrl', parent=self, pos=wxPoint(80, 80),
-              size=wxSize(96, 24), style=0, value='')
+              size=wxSize(96, 24), style=0, value=self.user)
 
         self.password_ctrl = wxTextCtrl(id=wxID_ATTACHDLGPASSWORD_CTRL,
               name='password_ctrl', parent=self, pos=wxPoint(80, 112),
-              size=wxSize(96, 24), style=wxTE_PASSWORD, value='')
+              size=wxSize(96, 24), style=wxTE_PASSWORD, value=self.pwd)
 
         self.ok_button = wxButton(id=wxID_ATTACHDLGOK_BUTTON, label='Ok',
               name='ok_button', parent=self, pos=wxPoint(24, 160),
@@ -76,7 +81,27 @@ class AttachDlg(wxDialog):
         EVT_BUTTON(self.help_button, wxID_ATTACHDLGHELP_BUTTON,
               self.OnHelpButton)
 
+        self.rem_pass_ctrl = wxCheckBox(id=wxID_ATTACHDLGREM_PASS_CTRL,
+              label='Remember', name='rem_pass_ctrl', parent=self,
+              pos=wxPoint(184, 120), size=wxSize(73, 13), style=0)
+        self.rem_pass_ctrl.SetValue(False)
+
     def __init__(self, editor):
+        host, port, user, pwd = \
+              self.rem_host, self.rem_port, self.rem_user, self.rem_pass
+
+        self.host = '127.0.0.1'
+        if host is not None: self.host = host
+
+        self.port = '26200'
+        if port is not None: self.port = port
+        
+        self.user = ''
+        if user is not None: self.user = user
+        
+        self.pwd = ''
+        if pwd is not None: self.pwd = pwd
+        
         self._init_ctrls(editor)
         self.editor = editor
 
@@ -86,6 +111,16 @@ class AttachDlg(wxDialog):
         user = self.username_ctrl.GetValue()
         pw = self.password_ctrl.GetValue()
 
+        AttachDlg.rem_host = host
+        AttachDlg.rem_port = port
+        AttachDlg.rem_user = user
+        if self.rem_pass_ctrl.GetValue():
+            AttachDlg.rem_pass = pw
+        else:
+            AttachDlg.rem_pass = None
+
+        self.Close()
+        return
         from Debugger import DebuggerFrame
         from RemoteClient import RemoteClient
 
@@ -100,10 +135,11 @@ class AttachDlg(wxDialog):
         self.editor.debugger = debugger
         debugger.doDebugStep()
         debugger.Show(true)
-        self.Close()
+        self.EndModal(wxOK)
 
     def OnCancelButton(self, event):
-        self.Close()
+        #self.Close()
+        self.EndModal(wxCANCEL)
 
     def OnHelpButton(self, event):
         pass
@@ -112,3 +148,15 @@ class AttachDlg(wxDialog):
         self.editor = None
         self.Destroy()
         event.Skip()
+
+
+if __name__ == '__main__':
+    app = wxPySimpleApp()
+    wxInitAllImageHandlers()
+    dlg = create(None)
+    try:
+        dlg.ShowModal()
+    finally:
+        dlg.Destroy()
+
+    app.MainLoop()
