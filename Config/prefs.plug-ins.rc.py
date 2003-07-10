a80917786a56a3e9a59fff09df1735d96e26bffd
riaan <none@none>
2003-07-10 13:27:55 +0000

    Moved resource config files from the root to the Config directory. Added mac rc files.

---
+++ Config/prefs.plug-ins.rc.py
@@ -0,0 +1,12 @@
+#-----------------------------------------------------------------------------
+# Name:        prefs.plug-ins.rc.py
+# Purpose:     A module for storing Plug-in file preferences
+#
+# Created:     2003
+# RCS-ID:      $Id$
+#-----------------------------------------------------------------------------
+
+""" A module for storing Plug-in file preferences """
+
+from wxPython.wx import *
+
