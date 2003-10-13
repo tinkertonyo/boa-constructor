4792ec2ff3170b70a99781a889ebc4685206ad62
riaan <none@none>
2003-10-13 09:17:09 +0000

    Removed all special casing. This module will be removed soon.

--- popen2import.py
+++ popen2import.py
@@ -1,18 +1,6 @@
 from wxPython import wx
-import sys
+import popen2
 
-if sys.version[:2] == '2.':
-    from os import popen3
-elif wx.wxPlatform == '__WXMSW__':
-
-    try:
-        from win32pipe import popen3
-    except ImportError:
-        wx.wxMessageBox('Please install Mark Hammond''s win32all package',
-              'Import Error', wx.wxOK | wx.wxICON_EXCLAMATION)
-        raise
-else:
-    def popen3(cmd):
-        import popen2
-        o, i, e = popen2.popen3(cmd)
-        return i, o, e
+def popen3(cmd):
+    inst = popen2.Popen3(cmd)
+    return inst.fromchild, inst.tochild, inst.childerr, inst.pid
