151cf175ee3bd5c94f118a77e2d8232b5bf28157
riaan <none@none>
2004-08-16 12:50:05 +0000

    Removed wxToolBarSimple.

--- Companions/ContainerCompanions.py
+++ Companions/ContainerCompanions.py
@@ -6,7 +6,7 @@
 #
 # Created:     2002
 # RCS-ID:      $Id$
-# Copyright:   (c) 2002 - 2003
+# Copyright:   (c) 2002 - 2004
 # Licence:     GPL
 #-----------------------------------------------------------------------------
 print 'importing Companions.ContainerCompanions'
@@ -1044,7 +1044,7 @@ PaletteStore.paletteLists['ContainersLayout'].extend([wxPanel, wxScrolledWindow,
 
 PaletteStore.compInfo.update({
     wxToolBar: ['wxToolBar', ToolBarDTC],
-    wxToolBarSimple: ['wxToolBarSimple', ToolBarSimpleDTC],
+    #wxToolBarSimple: ['wxToolBarSimple', ToolBarSimpleDTC],
     wxStatusBar: ['wxStatusBar', StatusBarDTC],
     wxPanel: ['wxPanel', PanelDTC],
     wxScrolledWindow: ['wxScrolledWindow', ScrolledWindowDTC],
