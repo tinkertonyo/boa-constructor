4fa8242d783294eba37734ed43cfb01b29970574
riaan <none@none>
2004-08-16 13:18:53 +0000

    Updated copyright date.

--- Debugger/IsolatedDebugger.py
+++ Debugger/IsolatedDebugger.py
@@ -7,7 +7,7 @@
 #
 # Created:      November 2000
 # RCS-ID:       $Id$
-# Copyright:    (c) Shane Hathaway, Riaan Booysen
+# Copyright:    (c) 2000 - 2004 : Shane Hathaway, Riaan Booysen
 # Licence:      GPL
 #----------------------------------------------------------------------------
 
