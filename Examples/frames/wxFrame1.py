7ee3e2a6acdc85f0a6f307878a1b81c23e7f2b9d
riaan <none@none>
2004-08-16 13:00:18 +0000

    Added examples of wizards.

--- Examples/frames/wxFrame1.py
+++ Examples/frames/wxFrame1.py
@@ -5,21 +5,17 @@ from wxPython.wx import *
 def create(parent):
     return wxFrame1(parent)
 
-[wxID_WXFRAME1, wxID_WXFRAME1BUTTON1, wxID_WXFRAME1BUTTON2,
-] = map(lambda _init_ctrls: wxNewId(), range(3))
+[wxID_WXFRAME1, wxID_WXFRAME1BUTTON1, wxID_WXFRAME1BUTTON2, 
+ wxID_WXFRAME1BUTTON3, 
+] = map(lambda _init_ctrls: wxNewId(), range(4))
 
 class wxFrame1(wxFrame):
-    def _init_utils(self):
-        # generated method, don't edit
-        pass
-
     def _init_ctrls(self, prnt):
         # generated method, don't edit
         wxFrame.__init__(self, id=wxID_WXFRAME1, name='', parent=prnt,
-              pos=wxPoint(299, 227), size=wxSize(370, 114),
+              pos=wxPoint(299, 227), size=wxSize(370, 146),
               style=wxDEFAULT_FRAME_STYLE, title='wxFrame1')
-        self._init_utils()
-        self.SetClientSize(wxSize(362, 87))
+        self.SetClientSize(wxSize(362, 119))
 
         self.button1 = wxButton(id=wxID_WXFRAME1BUTTON1, label='Show Frame 2',
               name='button1', parent=self, pos=wxPoint(112, 16),
@@ -30,6 +26,11 @@ class wxFrame1(wxFrame):
               name='button2', parent=self, pos=wxPoint(112, 48),
               size=wxSize(152, 23), style=0)
         EVT_BUTTON(self.button2, wxID_WXFRAME1BUTTON2, self.OnButton2Button)
+
+        self.button3 = wxButton(id=wxID_WXFRAME1BUTTON3, label='Show Wizard',
+              name='button3', parent=self, pos=wxPoint(112, 80),
+              size=wxSize(152, 23), style=0)
+        EVT_BUTTON(self.button3, wxID_WXFRAME1BUTTON3, self.OnButton3Button)
 
     def __init__(self, parent):
         self._init_ctrls(parent)
@@ -48,3 +49,24 @@ class wxFrame1(wxFrame):
         """ Example of a dynamic unreferenced frame """
         import wxFrame3
         wxFrame3.create(self).Show(true)
+
+    def OnButton3Button(self, event):
+        """ Example of calling a wxWizard """
+        import wxWizard123
+
+        if wxWizard1.run(self):
+            wxMessageBox('Wizard completed')
+        else:
+            wxMessageBox('Wizard cancelled')
+        
+        
+
+
+if __name__ == '__main__':
+    app = wxPySimpleApp()
+    wxInitAllImageHandlers()
+    frame = create(None)
+    # needed when running from Boa under Windows 9X
+    frame.Show();frame.Hide();frame.Show()
+
+    app.MainLoop()
