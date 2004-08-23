14d0d3a57c4fbf887b54008987b2c07f01261f88
riaan <none@none>
2004-08-23 12:36:21 +0000

    Improved the product builing script

--- Debugger/BoaDebugger/BuildProduct.py
+++ Debugger/BoaDebugger/BuildProduct.py
@@ -9,16 +9,16 @@ productFiles = [
       '__init__.py', 'help/debugger.stx', 'www/boa.gif']
 
 def main():
-    zipped = zipfile.ZipFile('BoaDebuggerProduct.zip', 'w')
-    zipped.write('../__init__.py', 'Boa/ExternalLib/__init__.py')
+    zipped = zipfile.ZipFile('BoaDebuggerProduct.zip', 'w', zipfile.ZIP_DEFLATED)
+    zipped.write('../__init__.py', 'BoaDebugger/Boa/ExternalLib/__init__.py')
     zipped.write('../../ExternalLib/xmlrpcserver.py', 
-                'Boa/ExternalLib/xmlrpcserver.py')    
+                'BoaDebugger/Boa/ExternalLib/xmlrpcserver.py')    
 
     for fn in boaSrcModules:
-        zipped.write('../%s.py'%fn, 'Boa/%s.py'%fn)    
+        zipped.write('../%s.py'%fn, 'BoaDebugger/Boa/%s.py'%fn)    
 
     for fn in productFiles:
-        zipped.write(fn, fn)    
+        zipped.write(fn, 'BoaDebugger/'+fn)    
 
 if __name__ == '__main__':
     main()        
