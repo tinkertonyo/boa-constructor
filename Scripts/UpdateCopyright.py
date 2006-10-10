a74476adf056f55aa7cd23f100cfecc1b81f9a44
riaan <none@none>
2006-10-10 09:51:18 +0000

    Source copyright updater

---
+++ Scripts/UpdateCopyright.py
@@ -0,0 +1,22 @@
+import os
+
+def update_copyright(old_date='2005', new_date='2006'):
+    os.chdir('..') # Boa root
+    for p, d, files in os.walk('.'):
+        for f in files:
+            f = os.path.join(p, f)
+            if not f.endswith('.py'):
+                continue
+            src = open(f).read()
+            c = src.find('# Copyright:   ')
+            if c != -1:
+                eol = src.find('\n', c)
+                d = src[c:eol].find(old_date)
+                if d != -1:
+                    src = src[:c+d]+new_date+src[c+d+len(new_date):]
+                    open(f, 'w').write(src)
+                    print 'updated', f
+            
+
+if __name__ == '__main__':
+    update_copyright()
