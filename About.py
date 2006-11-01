c2ddd3bfe40355c1b30a1ff5f703f8730df15101
riaan <none@none>
2006-11-01 18:26:29 +0000

    Added credit for the spanish translation

--- About.py
+++ About.py
@@ -80,6 +80,7 @@ German - Werner F. Bruhin (werner.bruhin@free.fr)<br>
 Italian - Michele Petrazzo (michele.petrazzo@unipex.it)<br>
 Brazilian Portuguese - Sergio Brant (sergiobrant@yahoo.com.br)<br>
 Afrikaans - Riaan Booysen (riaan@e.co.za)<br>
+Spanish - Felix Medrano Sanz (xukosky@yahoo.es)<br>
 
 <p>
 <b>Many thanks to</b><br>
